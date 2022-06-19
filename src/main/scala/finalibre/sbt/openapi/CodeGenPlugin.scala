df4712f0d87b5841fc6fbb7acf5cbf07b068aa12
Sune Rønne <surn@nykredit.dk>
2022-06-19 22:20:37 +0200

    Mapped up the added parameters

--- src/main/scala/finalibre/sbt/openapi/CodeGenPlugin.scala
+++ src/main/scala/finalibre/sbt/openapi/CodeGenPlugin.scala
@@ -9,7 +9,98 @@ object CodeGenPlugin extends AutoPlugin {
   override def trigger = allRequirements
   override def requires =JvmPlugin
 
+
+
   object autoImport {
+
+
+    val openApiAuthorization : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Authorization header when fetching Open API definitions of form: name:header with header as a comma-separated list of values"
+    )
+
+    val openApiAdditionalProperties : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Addition properties for Open API generation"
+    )
+
+    val openApiPackage : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Package name for generated client"
+    )
+
+    val openApiArtifactVersion : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Version # for generated client"
+    )
+
+    val openApiConfigFile : SettingKey[Option[File]] = settingKey[Option[File]](
+      "Optional config file for specifying properties of generation"
+    )
+
+    val openApiGitRepoId : SettingKey[Option[String]] = settingKey[Option[String]](
+      "ID of Git project"
+    )
+    val openApiGitUserId : SettingKey[Option[String]] = settingKey[Option[String]](
+      "User ID for Git connection"
+    )
+    val openApiGroupId : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Group ID"
+    )
+    val openApiHttpUserAgent : SettingKey[Option[String]] = settingKey[Option[String]](
+      "User agent for open API generation"
+    )
+    val openApiIgnoreFileOverwrite : SettingKey[Boolean] = settingKey[Boolean](
+      "Ignore file overwrites during open API generation?"
+    )
+
+
+    val openApiImportMappings : SettingKey[Option[String]] = settingKey[Option[String]](
+
+      "Import mappings for Open API generation"
+    )
+    val openApiInstantiationTypes : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Instantiation types for Open API generation"
+    )
+    val openApiInvokerPackage : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Package for generated Open API invoker"
+    )
+    val openApiLanguageSpecificPrimitives : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Language specific primitives for Open API generation"
+    )
+    val openApiLibrary : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Library for Open API generation"
+    )
+    val openApiModelNamePrefix : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Prefix name used for generated Open API model classes"
+    )
+    val openApiModelNameSuffix : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Suffix name used for generated Open API model classes"
+    )
+    val openApiModelPackage : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Package name used for generated Open API model classes"
+    )
+
+    val openApiReleaseNote : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Release note for generated Open API client"
+    )
+    val openApiRemoveOperationIdPrefix : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Remove operation id prefix"
+    )
+    val openApiReservedNameMappings : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Reserved name mappings for generated Open API client"
+    )
+    val openApiSkipOverwrite : SettingKey[Boolean] = settingKey[Boolean](
+      "Skip overwrite during open API generation?"
+    )
+
+    val openApiTemplatesDirectory : SettingKey[Option[File]] = settingKey[Option[File]](
+      "Template directory used for generated Open API client"
+    )
+    val openApiTypeMappings : SettingKey[Option[String]] = settingKey[Option[String]](
+      "Type mappings used for generated Open API client"
+    )
+    val openApiVerbose : SettingKey[Boolean] = settingKey[Boolean](
+      "Verbose during Open API generation"
+    )
+
+
 
     val openApiSpecification : SettingKey[java.io.File] = settingKey[java.io.File](
       "The file containing the swagger json file to use for generating client"
@@ -35,9 +126,40 @@ object CodeGenPlugin extends AutoPlugin {
   import autoImport._
 
   override lazy val globalSettings = Seq(
-    (Compile / openApiClientOutputFolder) := None,
+    openApiClientOutputFolder := None,
+    openApiAuthorization := None,
+    openApiAdditionalProperties := None,
+    openApiPackage := None,
+    openApiArtifactVersion := None,
+    openApiConfigFile := None,
+    openApiGitRepoId := None,
+    openApiGitUserId := None,
+    openApiGroupId := None,
+    openApiHttpUserAgent := None,
+    openApiIgnoreFileOverwrite := false,
+    openApiImportMappings := None,
+    openApiInstantiationTypes := None,
+    openApiInvokerPackage := None,
+    openApiLanguageSpecificPrimitives := None,
+    openApiModelNamePrefix := None,
+    openApiModelNameSuffix := None,
+    openApiModelPackage := None,
+    openApiPackage := None,
+    openApiReleaseNote := None,
+    openApiRemoveOperationIdPrefix := None,
+    openApiReservedNameMappings := None,
+    openApiSkipOverwrite := false,
+    openApiVerbose := false,
+    openApiLibrary := None,
+    openApiTemplatesDirectory := None,
+    openApiTypeMappings := None,
+
     javaOptions ++= Seq("--add-exports=swagger-codegen-cli/=ALL_UNNAMED"),
       libraryDependencies +=   "io.swagger.codegen.v3" % "swagger-codegen-cli" % "3.0.34"
+
+
+
+
   )
 
   override lazy val projectSettings = Seq(
@@ -50,7 +172,39 @@ object CodeGenPlugin extends AutoPlugin {
       val cliCodegenJar = (Compile / fullClasspath).value.find(_.data.getName.toLowerCase.matches("(?i)(swagger-codegen-cli.*jar)"))
       val swaggerFile = openApiSpecification.value
       val javaCommand = (Compile / javaHome).value.map(_.getAbsolutePath).getOrElse("java")
-      runSwaggerPlugin(swaggerFile, outDir, openApiClientLanguage.value, javaCommand , cliCodegenJar.get.data.getAbsolutePath)
+      runSwaggerPlugin(
+        swaggerFile,
+        outDir,
+        openApiClientLanguage.value,
+        javaCommand ,
+        cliCodegenJar.get.data.getAbsolutePath,
+        openApiAuthorization.value,
+        openApiAdditionalProperties.value,
+        openApiPackage.value,
+        openApiArtifactVersion.value,
+        openApiConfigFile.value,
+        openApiGitRepoId.value,
+        openApiGitUserId.value,
+        openApiGroupId.value,
+        openApiHttpUserAgent.value,
+        openApiIgnoreFileOverwrite.value,
+        openApiImportMappings.value,
+        openApiInstantiationTypes.value,
+        openApiInvokerPackage.value,
+        openApiLanguageSpecificPrimitives.value,
+        openApiLibrary.value,
+        openApiModelNamePrefix.value,
+        openApiModelNameSuffix.value,
+        openApiModelPackage.value,
+        openApiReleaseNote.value,
+        openApiRemoveOperationIdPrefix.value,
+        openApiReservedNameMappings.value,
+        openApiSkipOverwrite.value,
+        openApiTemplatesDirectory.value,
+        openApiTypeMappings.value,
+        openApiVerbose.value
+
+      )
     }
   )
 
@@ -66,7 +220,6 @@ object CodeGenPlugin extends AutoPlugin {
                         apiPackage : Option[String],
                         artifactVersion : Option[String],
                         codegenConfigFile : Option[File],
-                        systemProperties : Option[String],
                         gitRepoId : Option[String],
                         gitUserId : Option[String],
                         groupId : Option[String],
@@ -109,7 +262,6 @@ object CodeGenPlugin extends AutoPlugin {
       "--api-package" -> apiPackage,
       "--artifact-version" -> artifactVersion,
       "-c" -> codegenConfigFile,
-      "-D" -> systemProperties,
       "--git-repo-id" -> gitRepoId,
       "--git-user-id" -> gitUserId,
       "--group-id" -> groupId,
@@ -126,9 +278,12 @@ object CodeGenPlugin extends AutoPlugin {
       "--remove-operation-id-prefix" -> removeOperationIdPrefix,
       "--reserved-words-mappings" -> reservedWordsMappings,
       "-t" -> templatesDirectory.map(_.getAbsolutePath) ,
-     "--type-mappings" -> typeMappings ]
+     "--type-mappings" -> typeMappings
+    ).collect {
+      case (nam, Some(value)) => nam -> value
+    }
 
-    )
+    val execArgs = mandatoryArgs ++ optionalArgs
 
     if(outputDir.exists)
       outputDir.delete()
